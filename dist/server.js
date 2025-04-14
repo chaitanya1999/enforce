a0565feea46f9292b2d4027bbb39039dc862c3f2
Chaitanya V <cnjv1999@gmail.com>
2025-04-14 21:02:40 +0530

    caching assets

--- dist/server.js
+++ dist/server.js
@@ -4,17 +4,18 @@ const app = express()
 const port = process.env.PORT || 80
 let env = process.env.NODE_ENV || 'development';
 let staticOptions = {
-	maxAge : 86400000
+	maxAge : 86400000,
+	etag : true
 }
 let jsforceCorsProxy = jsforceAjaxProxy({enableCORS : (env == 'PROD' ? false : true)});
 
 app.get('/:file', (req, res) => {
-	console.log(req.params.file);
+	console.log('#$#$ Request = ' + req.params.file);
  
 	if(!req.params.file) {
 		res.sendFile('/web/index.html', { root: __dirname });
 	} else if(req.params.file.startsWith('proxy')) {
-		console.log('proxy proxy #$#$');
+		console.log('#$#$ Proxy request');
 		jsforceCorsProxy(req, res);	
 	} else {
 		res.sendFile('/web/' + req.params.file, { root: __dirname });
@@ -26,7 +27,7 @@ app.get('/', (req,res) => {
 
 app.use('/assets/', express.static(__dirname + '/web/assets/', staticOptions));
 
-app.use('/assets/monaco/vs/', express.static(__dirname + '/web/assets/monaco/vs/', staticOptions));
+app.use('/assets/monaco/vs/', express.static(__dirname + '/web/assets/monaco/vs/'));
 
 app.all('/proxy/?*', jsforceCorsProxy);
 
